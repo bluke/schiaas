be8d7344c1e710d8b51bbe9409af0d25e970fbdf
Julien <gossa@unistra.fr>
2014-05-28 18:09:37 +0200

    cute stats

--- validation/stats/stats.R
+++ validation/stats/stats.R
@@ -3,73 +3,69 @@
 stats <- read.table("stats.dat", header=TRUE)
 
 btu <- stats[c("source","BTU","wpo.BTU","wto.BTU")]
-
 btu$wpo.d <- NA
 btu$wpo.d <- abs( btu$BTU - btu$wpo.BTU ) / btu$BTU
-pdf('btu-cdf-wpo.pdf')
-plot(ecdf(btu$wpo.d))
-dev.off()
-
 btu$wto.d <- NA
 btu$wto.d <- abs( btu$BTU - btu$wto.BTU ) / btu$BTU
-
-pdf('btu-cdf-wto.pdf')
-plot(ecdf(btu$wto.d))
-dev.off()
-
-pdf('btu-cdf-wpo-openstack.pdf')
-plot(ecdf(btu[grep("openstack",btu$source),]$wto.d))
-dev.off()
-
-pdf('btu-cdf-wpo-bonfire.pdf')
-plot(ecdf(btu[grep("bonfire",btu$source),]$wto.d))
-dev.off()
-
-pdf('btu-cdf-wpo-montage.pdf')
-plot(ecdf(btu[grep("pleiade",btu$source),]$wto.d))
-dev.off()
-
-pdf('btu-cdf-wpo-omssa.pdf')
-plot(ecdf(btu[grep("pleiade",btu$source,invert=TRUE),]$wto.d))
-dev.off()
-
-
 write.table(btu[ with(btu, order(wpo.d)), ], file="btu.dat")
 
 
 makespan <- stats[c("source","makespan","wpo.makespan","wto.makespan")]
 
+
 makespan$wpo.d <- NA
 makespan$wpo.d <- abs( makespan$makespan - makespan$wpo.makespan ) / makespan$makespan
-pdf('makespan-cdf-wpo.pdf')
-plot(ecdf(makespan$wpo.d))
-dev.off()
-
 makespan$wto.d <- NA
 makespan$wto.d <- abs( makespan$makespan - makespan$wto.makespan ) / makespan$makespan
-
-pdf('makespan-cdf-wto.pdf')
-plot(ecdf(makespan$wto.d))
-dev.off()
-
-pdf('makespan-cdf-wpo-openstack.pdf')
-plot(ecdf(makespan[grep("openstack",makespan$source),]$wto.d))
-dev.off()
-
-pdf('makespan-cdf-wpo-bonfire.pdf')
-plot(ecdf(makespan[grep("bonfire",makespan$source),]$wto.d))
-dev.off()
-
-pdf('makespan-cdf-wpo-montage.pdf')
-plot(ecdf(makespan[grep("pleiade",makespan$source),]$wto.d))
-dev.off()
-
-pdf('makespan-cdf-wpo-omssa.pdf')
-plot(ecdf(makespan[grep("pleiade",makespan$source,invert=TRUE),]$wto.d))
-dev.off()
-
-
 write.table(makespan[ with(makespan, order(wpo.d)), ], file="makespan.dat")
+
+
+
+pdf('global-cdf.pdf')
+plot(ecdf(btu$wpo.d),verticals=TRUE, do.p=FALSE, lty=1, col="red", 
+	main="Global accuracy", xlab="accuracy", ylab="amount of cases")
+lines(ecdf(btu$wto.d),verticals=TRUE, do.p=FALSE, lty=3, col="red")
+lines(ecdf(makespan$wpo.d),verticals=TRUE, do.p=FALSE, lty=1, col="blue")
+lines(ecdf(makespan$wto.d),verticals=TRUE, do.p=FALSE, lty=3, col="blue")
+legend(0.7,0.3,c("btu wpo","btu wto","makespan wpo","makespan wto"),lty=c(1,3,1,3),col=c("red","red","blue","blue"))
+dev.off()
+
+pdf('expes-btu-cdf.pdf')
+plot(ecdf(btu[grep("pleiade",btu$source),]$wpo.d),verticals=TRUE, do.p=FALSE, lty=1, col="red",
+	main="BTU accuracy per xp", xlab="accuracy", ylab="amount of cases")
+lines(ecdf(btu[grep("pleiade",btu$source),]$wto.d),verticals=TRUE, do.p=FALSE, lty=3, col="red")
+lines(ecdf(btu[grep("pleiade",btu$source,invert=TRUE),]$wpo.d),verticals=TRUE, do.p=FALSE, lty=1, col="blue")
+lines(ecdf(btu[grep("pleiade",btu$source,invert=TRUE),]$wto.d),verticals=TRUE, do.p=FALSE, lty=3, col="blue")
+legend(0.7,0.3,c("montage btu wpo","montage btu wto","omssa btu wpo","omssa btu wto"),lty=c(1,3,1,3),col=c("red","red","blue","blue"))
+dev.off()
+
+pdf('cloud-btu-cdf.pdf')
+plot(ecdf(btu[grep("openstack",btu$source),]$wpo.d),verticals=TRUE, do.p=FALSE, lty=1, col="red",
+	main="BTU accuracy per cloud", xlab="accuracy", ylab="amount of cases")
+lines(ecdf(btu[grep("openstack",btu$source),]$wto.d),verticals=TRUE, do.p=FALSE, lty=3, col="red")
+lines(ecdf(btu[grep("bonfire",btu$source),]$wpo.d),verticals=TRUE, do.p=FALSE, lty=1, col="blue")
+lines(ecdf(btu[grep("bonfire",btu$source),]$wto.d),verticals=TRUE, do.p=FALSE, lty=3, col="blue")
+legend(0.7,0.3,c("icps btu wpo","icps btu wto","bonfire btu wpo","bonfire btu wto"),lty=c(1,3,1,3),col=c("red","red","blue","blue"))
+dev.off()
+
+pdf('expes-makespan-cdf.pdf')
+plot(ecdf(makespan[grep("pleiade",makespan$source),]$wpo.d),verticals=TRUE, do.p=FALSE, lty=1, col="red",
+	main="Makespan accuracy per xp", xlab="accuracy", ylab="amount of cases")
+lines(ecdf(makespan[grep("pleiade",makespan$source),]$wto.d),verticals=TRUE, do.p=FALSE, lty=3, col="red")
+lines(ecdf(makespan[grep("pleiade",makespan$source,invert=TRUE),]$wpo.d),verticals=TRUE, do.p=FALSE, lty=1, col="blue")
+lines(ecdf(makespan[grep("pleiade",makespan$source,invert=TRUE),]$wto.d),verticals=TRUE, do.p=FALSE, lty=3, col="blue")
+legend(0.7,0.3,c("montage makespan wpo","montage makespan wto","omssa makespan wpo","omssa makespan wto"),lty=c(1,3,1,3),col=c("red","red","blue","blue"))
+dev.off()
+
+pdf('cloud-makespan-cdf.pdf')
+plot(ecdf(makespan[grep("openstack",makespan$source),]$wpo.d),verticals=TRUE, do.p=FALSE, lty=1, col="red",
+	main="Makespan accuracy per cloud", xlab="accuracy", ylab="amount of cases")
+lines(ecdf(makespan[grep("openstack",makespan$source),]$wto.d),verticals=TRUE, do.p=FALSE, lty=3, col="red")
+lines(ecdf(makespan[grep("bonfire",makespan$source),]$wpo.d),verticals=TRUE, do.p=FALSE, lty=1, col="blue")
+lines(ecdf(makespan[grep("bonfire",makespan$source),]$wto.d),verticals=TRUE, do.p=FALSE, lty=3, col="blue")
+legend(0.7,0.3,c("icps makespan wpo","icps makespan wto","bonfire makespan wpo","bonfire makespan wto"),lty=c(1,3,1,3),col=c("red","red","blue","blue"))
+dev.off()
+
 
 
 min(btu$wpo.d)
