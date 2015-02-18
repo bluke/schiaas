f080a54639daf68d5fbd0c6351ebd69fa042cb46
Julien Gossa <julien.gossa@unistra.fr>
2015-02-18 11:38:49 +0100

    missing files

---
+++ examples/simschlouder/util/SimSchlouderException.java
@@ -0,0 +1,16 @@
+package simschlouder.util;
+
+/**
+ * Exceptions for SimSchlouder
+ * @author julien
+ *
+ */
+public class SimSchlouderException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public SimSchlouderException(String message) {
+		super(message);
+		
+	}
+}
