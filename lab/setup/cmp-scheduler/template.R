e16827fa9e8d63077949d4d357eca33979da6f89
Julien Gossa <julien.gossa@unistra.fr>
2015-11-10 16:19:55 +0100

    trace-util count-if

--- lab/setup/cmp-scheduler/template.R
+++ lab/setup/cmp-scheduler/template.R
@@ -7,3 +7,10 @@ source('plots.R')
 
 dev.off()
 
+#valueat <- function(v,d) { return(tail(d[d$date<=v,],1)$value) }
+
+#va_migrations <- function(v) {
+#	return(valueat(v, reconsolidator100.VirtualMachines__state_eq_migrating)[1])
+#}
+
+#integrate(va_migrations, 0, 10000)
