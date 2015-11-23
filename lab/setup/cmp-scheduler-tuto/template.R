a88cdf1b27cabf5a8783225df9f6e3dadf04fbf2
Julien Gossa <julien.gossa@unistra.fr>
2015-11-23 19:17:02 +0100

    details details details

--- lab/setup/cmp-scheduler-tuto/template.R
+++ lab/setup/cmp-scheduler-tuto/template.R
@@ -18,6 +18,7 @@ dev.off()
 ###################################### specific
 
 png('busy_hosts-integral-slow.png', width=700)
+par(mar=c(12,5,1,1)) 
 subxps <- data.frame(xp=xps[grep('fast', xps$xp, invert=TRUE),])
 uc <-tu_apply(subxps,'used_cores_ne_0',tu_integrate)
 barplot(uc$value, names.arg=uc$xp, las=2)
@@ -25,6 +26,7 @@ print(uc)
 dev.off()
 
 png('busy_hosts-integral-fast.png', width=700)
+par(mar=c(12,5,1,1)) 
 subxps <- data.frame(xp=xps[grep('_fast', xps$xp),])
 uc <-tu_apply(subxps,'used_cores_ne_0',tu_integrate)
 barplot(uc$value, names.arg=uc$xp, las=2)
