6403c474e90c647cf5ce1927cee7df903821eace
Luke Bertot <lbertot@unistra.fr>
2016-05-27 16:13:42 +0200

    A better, faster, stronger lab with lab.py
    lab.py can now be used in stead of lab.sh
    	+ crossplatform
    	+ does not realy on shell-based black wizardry
    	+ reorders SIM_ARGs to make full use of includes
    	- requiers NEEDED_POST for files needed by POST_COMMAND_DATA
    	(can't understand how the lab.sh did it, surely black magic)
    
    updated cmp-scheduler.cfg as an example
    
    updated generate_sim.py because for some reason simschlouder dosen't
    take jobs with negative runtimes. (and to be more style compliant)

--- lab/lab.py
+++ lab/lab.py
@@ -1,56 +1,124 @@
 #!/usr/bin/python3
+#pylint: disable=too-many-branches, line-too-long, consider-using-enumerate
+"""
+This script executes simulations according to one config file.
+It takes at least one configuration file as argument.
+It needs the java classpath to be set.
 
+The configuration file format is as follows:
+SETUP_DIR indicates the directory containing setup files (should be called first)
+INCLUDE indicates a file to read as addtional configuration file
+NEEDED indicates file or directory that are to be in the simuation directory
+NEEDED_POST indicates file or directory that are to be in the data directory
+PRE_COMMAND_SETUP indicates a command to run before the simulation in the setup directory
+POST_COMMAND_DATA indicates a command to run in the data directory
+TU_ARG indicates the arguments to pass to the trace_util.py script
+SIM_ARG x[:id] arg indicates the argument of the simulation
+ - x: the id of the argument (must be grouped);
+ - id: the id of the simulations using this argument;
+ - arg: the argument.
+
+see lab/setup/cmp-scheduler/cmp-scheduler.cfg for example
+Two options are available:
+-k : keeps the data from previous executions
+-p x : run x simulations and x trace-util.py in parallel,
+
+@author julien.gossa@unistra.fr
+@author lbertot@unistra.fr
+"""
 """
 Error exit codes
 1 : config file not found
 2 : SIM_ARG order number is not a number
-3 : failed to resolve a path
+3 : failed to find Setup dir
+4 : failed to find included file.
 """
 
 import argparse
+import datetime
+import itertools
 import os
+import queue
+import shutil
 import subprocess
 import sys
+try:
+    import threading
+except ImportError:
+    print("Threading module missing using dummy threading.", file=sys.stderr)
+    import dummy_threading as threading
 
-def getPath(path,config):
-    if not config['setupdir'] == None and os.path.exists(os.path.join(config['setupdir'],path)):
-        return os.path.abspath(os.path.join(config['setupdir'],path))
+def get_path(path, config):
+    """
+    Gets the absolute path corresponding to a given path.
+
+    This function searches relative to the setupdir, the cwd, or the script file.
+
+    Args:
+        path: the path to look for
+        config: configuration containing the setupdir
+
+    Returns: the absolute path of the file found or None
+    """
+    if not config['setupdir'] is None and os.path.exists(os.path.join(config['setupdir'], path)):
+        return os.path.abspath(os.path.join(config['setupdir'], path))
     elif os.path.exists(path):
         return os.path.abspath(path)
-    elif os.path.exists(os.path.join(os.path.dirname(os.path.realpath(__file__)),path)):
-        return os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)),path))
+    elif os.path.exists(os.path.join(os.path.dirname(os.path.realpath(__file__)), path)):
+        return os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), path))
     else:
-        print("Error : file or directory '{0}' not found relative to setupdir,cwd,or script".format(path),file=sys.stderr)
-        sys.exit(3)
+        return None
 
-def parseSimArg(line):
+def parse_simarg(line):
+    """
+    Parse a SIMARG line form the config file
+    Arg: the tail of a SIMARG line from the cfg file
+    Returns: a SimArg dictionnary
+    """
     res = {}
     lst = line.split(maxsplit=1)
     res['argument'] = lst[1]
-    name = None
-    order = lst[0].split(':',maxsplit=1)
+    order = lst[0].split(':', maxsplit=1)
     try:
         res['index'] = int(order[0])
+    #pylint: disable=undefined-variable
     except ValueERROR:
-        print("Error : in SIM_ARG order '{0}' is not a digit".order[0],file=sys.stderr)
+        print("Error : in SIM_ARG order '{0}' is not a digit".format(order[0]), file=sys.stderr)
         sys.exit(2)
-    if len(order)==2:
-        res['name']=order[1]
+    if len(order) == 2:
+        res['name'] = order[1]
     return res
 
-def readFile(fileName,config):
+def read_cfg_file(filename, config):
+    """
+    Recursively parse content of cfg files, and adds content to dictionary.
+    Also executes PRE_COMMANDS
+
+    Args:
+        filename : path to file to parse
+        config : lab configuration dictionnary
+    """
     included = []
     precommandsetup = []
-    print("Parsing {0}".format(fileName),file=sys.stderr)
-    with open(fileName,'r') as f:
-        for readLine in f:
-            line = readLine.split(maxsplit=1)
+    print("Parsing {0}".format(filename), file=sys.stderr)
+    with open(filename, 'r') as cfg:
+        for readline in cfg:
+            line = readline.split(maxsplit=1)
             if len(line) == 0:
                 continue
-            elif line[0] == 'SETUP_DIR' and config['setupdir'] == None:
-                config['setupdir'] = getPath(line[1].rstrip(),config)
+            elif line[0] == 'SETUP_DIR' and config['setupdir'] is None:
+                givenpath = line[1].rstrip()
+                setdpath = get_path(givenpath, config)
+                if not setdpath is None and os.path.isdir(setdpath):
+                    config['setupdir'] = setdpath
+                else:
+                    print("Error : the SETUP_DIR '{0}' was not found.".format(givenpath),
+                          file=sys.stderr)
+                    sys.exit(3)
             elif line[0] == 'NEEDED':
-                config['needed']+= line[1].split()
+                config['needed'] += line[1].split()
+            elif line[0] == 'NEEDED_POST':
+                config['neededpost'] += line[1].split()
             elif line[0] == 'PRE_COMMAND_SETUP':
                 precommandsetup.append(line[1].rstrip())
             elif line[0] == 'POST_COMMAND_DATA':
@@ -58,52 +126,282 @@ def readFile(fileName,config):
             elif line[0] == 'TU_ARG':
                 config['tuarg'].append(line[1].rstrip())
             elif line[0] == 'SIM_ARG':
-                config['simarg'].append(parseSimArg(line[1].rstrip()))
+                config['simarg'].append(parse_simarg(line[1].rstrip()))
             elif line[0] == 'INCLUDE':
-                included.append(line[1].rstrip()) 
+                included.append(line[1].rstrip())
     for command in precommandsetup:
-        print("Running pre-command '{0}' in '{1}'".format(command,"." if config['setupdir']== None else config['setupdir']))
-        process = subprocess.Popen(command,shell=True,cwd=config['setupdir'])
+        print("Running pre-command '{0}' in '{1}'".format(command, "." if config['setupdir'] is None else config['setupdir']))
+        process = subprocess.Popen(command, shell=True, cwd=config['setupdir'])
         process.wait()
-    config['precommandsetup']+=precommandsetup
+    config['precommandsetup'] += precommandsetup
     for inc in included:
-        print("Including config file '{0}'".format(inc))        
-        readFile(getPath(inc,config),config)
+        includepath = get_path(inc, config)
+        if not includepath is None and os.path.isfile(includepath):
+            print("Including config file '{0}'".format(inc))
+            read_cfg_file(get_path(inc, config), config)
+        else:
+            print("Error : the INCLUDE file {0} can not be found".format(inc), file=sys.stderr)
+            sys.exit(4)
 
-def setDir(config):
+def set_dirs(config):
+    """
+    Sets directories in config.
+    """
     config['cdir'] = os.path.abspath('.')
     config['scriptdir'] = os.path.abspath(os.path.dirname(os.path.realpath(__file__)))
-    config['bindir'] = os.path.join(config['scriptdir'],"bin")
-    config['simdir'] = os.path.join(config['cdir'],"simulation")
-    config['datadir'] = os.path.join(config['cdir'],"data")
+    config['bindir'] = os.path.join(config['scriptdir'], "bin")
+    config['simdir'] = os.path.join(config['cdir'], "simulations")
+    config['datadir'] = os.path.join(config['cdir'], "data")
+    config['archdir'] = os.path.join(config['cdir'], "archive")
 
-def planExperiements(config):
-    pass
+def plan_experiments(config):
+    """
+    Prepares all possible combinations of experiements.
+    """
+    sort = []
+    for i in range(1, max(config['simarg'], key=lambda x: x['index'])['index']+1):
+        sort.append([x for x in config['simarg'] if x['index'] == i])
+    planner = list(itertools.product(*sort))
+    res = []
+    for i in planner:
+        name = 'xp'
+        command = []
+        for j in range(len(i)):
+            command.append(i[j]['argument'])
+            if 'name' in i[j]:
+                name += '_' + i[j]['name']
+        res.append((name, command))
+    return res
 
+def setup_simulation(in_queue, out_queue, config):
+    """
+    Sets up simulations dir of sims taken from queue
+
+    Args :
+        in_queue : queue from where to read simulation
+        out_queue : where to ouptput prepared experiment
+        config : the configuration object including the NEEDED files
+                and the simulation directory
+    """
+    while True:
+        exp = in_queue.get()
+        if exp is None:
+            break
+        print("Setting up simulation {0}".format(exp[0]))
+        exp_dir = os.path.join(config['simdir'], exp[0])
+        os.makedirs(exp_dir)
+        for need in config['needed']:
+            src_path = get_path(need, config)
+            dst_path = os.path.join(exp_dir, need)
+            if src_path is None:
+                print("Error NEEDED object '{0}' has not been found".format(need), file=sys.stderr)
+                out_queue.put(None)
+                return
+            elif os.path.isdir(src_path):
+                if not os.path.exists(dst_path):
+                    shutil.copytree(src_path, dst_path)
+            elif os.path.isfile(src_path):
+                if not os.path.exists(os.path.dirname(dst_path)):
+                    os.makedirs(os.path.dirname(dst_path))
+                shutil.copy(src_path, dst_path)
+            else:
+                print("Error NEEDED object '{0}' is not a file or a directory".format(need), file=sys.stderr)
+                out_queue.put(None)
+                return
+
+        for arg in exp[1]:
+            src_path = get_path(arg, config)
+            dst_path = os.path.join(exp_dir, arg)
+            if src_path is None:
+                pass
+            elif os.path.isdir(src_path):
+                if not os.path.exists(dst_path):
+                    shutil.copytree(src_path, dst_path)
+            elif os.path.isfile(src_path):
+                if not os.path.exists(os.path.dirname(dst_path)):
+                    os.makedirs(os.path.dirname(dst_path))
+                shutil.copy(src_path, dst_path)
+
+        out_queue.put(exp)
+    out_queue.put(None)
+
+def run_simulation(in_queue, out_queue, config):
+    """
+    Runs simulations from queue
+    """
+    while True:
+        exp = in_queue.get()
+        if exp is None:
+            break
+        exp_dir = os.path.join(config['simdir'], exp[0])
+        command = ['java']
+        command += exp[1]
+        print("Run simulation {0}".format(exp[0]))
+        with open(os.path.join(exp_dir, "simgrid.out"), 'w') as out_file:
+            process = subprocess.Popen(command, stdout=out_file, stderr=out_file, cwd=exp_dir)
+            process.wait()
+
+        if process.returncode == 0:
+            out_queue.put(exp)
+
+    out_queue.put(None)
+
+
+def extract_trace(in_queue, config):
+    """
+    Runs trace utils on experiments from queue
+    """
+    util = os.path.join(config['bindir'], "trace-util.py")
+    while True:
+        exp = in_queue.get()
+        if exp is None:
+            break
+        exp_dir = os.path.join(config['simdir'], exp[0])
+        print("Processing traces for {0}".format(exp[0]))
+        for tuarg in config['tuarg']:
+            command = [util, "schiaas.trace", "-o", config['datadir'], "-f", exp[0], tuarg]
+            process = subprocess.Popen(" ".join(command), shell=True, cwd=exp_dir)
+            process.wait()
+
+
+def clean_simdir(keep, config):
+    """
+    cleans the simdir
+
+    keep : bool whether to keep prevous results
+    """
+    if keep:
+        if os.path.exists(os.path.join(config['simdir'], 'simulations.args')):
+            with open(os.path.join(config['simdir'], 'simulations.args'), 'r') as simargs_file:
+                archname = simargs_file.readline().strip()
+            if not os.path.exists(config['archdir']):
+                os.makedirs(config['archdir'])
+            shutil.copytree(config['simdir'], os.path.join(config['archdir'], archname))
+
+    if os.path.exists(config['simdir']):
+        shutil.rmtree(config['simdir'])
+    if os.path.exists(config['datadir']):
+        shutil.rmtree(config['datadir'])
+    os.mkdir(config['simdir'])
+    os.mkdir(config['datadir'])
+
+def print_plan(plan, config):
+    """
+    Writes the simulations.args file
+    """
+    now = datetime.datetime.now()
+    file_path = os.path.join(config['simdir'], 'simulations.args')
+    with open(file_path, 'w') as simargs_file:
+        print("{}-{}-{}_{}{}".format(now.year, now.month, now.day, now.hour, now.minute), file=simargs_file)
+        for exp in plan:
+            print(exp[0], " ".join(exp[1]), file=simargs_file)
+
+def run_post_commands(config):
+    """
+    Runs post commands in the data dir
+    """
+    for need in config['neededpost']:
+        src_path = get_path(need, config)
+        dst_path = os.path.join(config['datadir'], need)
+        if src_path is None:
+            print("Error NEEDED_POST object '{0}' has not been found".format(need), file=sys.stderr)
+            return
+        elif os.path.isdir(src_path):
+            if not os.path.exists(dst_path):
+                shutil.copytree(src_path, dst_path)
+        elif os.path.isfile(src_path):
+            if not os.path.exists(os.path.dirname(dst_path)):
+                os.makedirs(os.path.dirname(dst_path))
+            shutil.copy(src_path, dst_path)
+        else:
+            print("Error NEEDED_POST object '{0}' is not a file or a directory".format(need), file=sys.stderr)
+            return
+
+    for command in config['postcommanddata']:
+        print("Running post-command {0} in {1}".format(command, config['datadir']))
+        process = subprocess.Popen(command, shell=True, cwd=config['datadir'])
+        process.wait()
+
+
+
+
+#pylint: disable=too-many-locals
 def main():
+    """
+    Main function of the program.
+    """
+
+    # parse arguments
     parser = argparse.ArgumentParser(description="Execute a simulation according to a config file")
-    parser.add_argument("-p",action="store",required=False,
-            default=1,dest="numParal",type=int,
-            help="Number of parallel simulations")
-    parser.add_argument("confFile",action="store",type=str,
-            help="Config file describing the experiment")
+    parser.add_argument("-k", action="store_true", dest="keep",
+                        help="Keep result for previous run")
+    parser.add_argument("-p", action="store", required=False,
+                        default=1, dest="numParal", type=int,
+                        help="Number of parallel simulations")
+    parser.add_argument("confFile", action="store", type=str,
+                        help="Config file describing the experiment")
     args = parser.parse_args()
 
- 
-    config={'setupdir':None,'needed':[],
-            'precommandsetup':[],'postcommanddata':[],
-            'tuarg':[],'simarg':[]}
-    setDir(config)
+    config = {'setupdir':None, 'needed':[], 'neededpost':[],
+              'precommandsetup':[], 'postcommanddata':[],
+              'tuarg':[], 'simarg':[]}
 
+    # set minimal config
+    set_dirs(config)
+
+    # cleanup previous configuration
+    clean_simdir(args.keep, config)
+
+    # load configuration file
     if not os.path.isfile(args.confFile):
-        print("Error : the config file '{0}' was not found.".format(args.confFile),file=sys.stderr)
+        print("Error : the config file '{0}' was not found.".format(args.confFile), file=sys.stderr)
         sys.exit(1)
     else:
-        readFile(args.confFile,config)
-              
-    print(max(config['simarg'],key=lambda x : x['index']))
-    print([x for x in config['simarg'] if x['index']==3])
+        read_cfg_file(args.confFile, config)
 
+    # plan simulations
+    plan = plan_experiments(config)
+    print_plan(plan, config)
+
+    # prepare pipelines
+    set_simspace_queue = queue.Queue(len(plan)+args.numParal+1)
+    run_sim_queue = queue.Queue(len(plan)+args.numParal+1)
+    trace_output_queue = queue.Queue(len(plan)+args.numParal+1)
+
+    for exp in plan:
+        set_simspace_queue.put(exp)
+    for _ in range(args.numParal):
+        set_simspace_queue.put(None)
+
+    # Run workers
+    setters = []
+    runners = []
+    tracers = []
+    for _ in range(args.numParal):
+        setters.append(threading.Thread(target=setup_simulation, args=(set_simspace_queue, run_sim_queue, config)))
+        runners.append(threading.Thread(target=run_simulation, args=(run_sim_queue, trace_output_queue, config)))
+    for setter in setters:
+        setter.start()
+    for runner in runners:
+        runner.start()
+
+    # waiting for setter
+    for setter in setters:
+        setter.join()
+
+    # launching analysers
+    for i in range(args.numParal):
+        tracers.append(threading.Thread(target=extract_trace, args=(trace_output_queue, config)))
+        tracers[i].start()
+
+    # Wait for sims and traces
+    for runner in runners:
+        runner.join()
+    for tracer in tracers:
+        tracer.join()
+
+    # run post commands
+    run_post_commands(config)
 
 if __name__ == '__main__':
     main()
--- lab/setup/simschlouder/generate_sim.py
+++ lab/setup/simschlouder/generate_sim.py
@@ -1,65 +1,63 @@
-#!/bin/python
-
+#!/usr/bin/python3
+#pylint: disable=invalid-name,missing-docstring,line-too-long
 import argparse
-import copy 
-import csv
+import copy
 import os
-import rpy2.robjects as robjects
 from rpy2.robjects.packages import importr
 
-def writeFile(fileName,data):
-    with open(fileName,'w') as f:
+def writeFile(fileName, data):
+    with open(fileName, 'w') as out_file:
         for line in data:
-            out ="{0}\t{1}\t{2}".format(line['id'],line['subDate'],line['predicted'])
+            out = "{0}\t{1}\t{2}".format(line['id'], line['subDate'], line['predicted'])
             if 'runtime' in line:
                 out += "\t~ {0}".format(line['runtime'])
             if ('inbound' in line) and ('outbound' in line):
-                out += "\t{0}\t{1}".format(line['inbound'],line['outbound'])
+                out += "\t{0}\t{1}".format(line['inbound'], line['outbound'])
             if 'dependencies' in line:
                 out += "\t->"
                 for s in line['dependencies']:
                     out += " "+s
-            out +="\n"
-            f.write(out)
+            out += "\n"
+            out_file.write(out)
 
 def loadLine(lineArray):
-    res={}
+    res = {}
     if len(lineArray) >= 3:
         itr = iter(lineArray)
-        res['id']=next(itr)
-        res['subDate']=int(next(itr))
-        res['predicted']=float(next(itr))
+        res['id'] = next(itr)
+        res['subDate'] = int(next(itr))
+        res['predicted'] = float(next(itr))
         if len(lineArray) >= 3:
             for v in itr:
                 if v == '~':
-                    res['runtime']=float(next(itr))
+                    res['runtime'] = float(next(itr))
                 elif v == '->':
                     deps = []
                     for d in itr:
                         deps.append(d)
-                    res['dependencies']=deps
+                    res['dependencies'] = deps
                 else:
-                    res['inbound']=float(v)
-                    res['outbound']=float(next(itr))
+                    res['inbound'] = float(v)
+                    res['outbound'] = float(next(itr))
     return res
 
 def loadFile(fileName):
     data = []
     if os.path.isfile(fileName):
-        with open(fileName,'r') as f:
+        with open(fileName, 'r') as f:
             for line in f:
                 dic = loadLine(line.split())
                 data.append(dic)
     return data
 
-def getRandomValues(size,func,expect,shape):
+def getRandomValues(size, func, expect, shape):
     stats = importr('stats')
-    res=[]
+    res = []
 
     if func == 'weibull':
-        lst = stats.rweibull(size,shape,expect)
+        lst = stats.rweibull(size, shape, expect)
     elif func == 'normal':
-        lst = stats.rnorm(size,expect,shape)
+        lst = stats.rnorm(size, expect, shape)
     else:
         lst = None
 
@@ -74,54 +72,54 @@ def base(dic):
     else:
         return dic['predicted']
 
-def updateData(inData,rVals,treat,expect):
-    res=[]
+def updateData(inData, rVals, treat, expect):
+    res = []
     r = iter(rVals)
     for line in inData:
         if treat == 'add':
-            line['runtime']=base(line)+next(r)
+            line['runtime'] = base(line)+next(r)
         elif treat == 'norm':
-            line['runtime']=base(line)*(expect/next(r))
+            line['runtime'] = base(line)*(expect/next(r))
         else:
-            line['runtime']=next(r)
+            line['runtime'] = next(r)
+        if line['runtime'] < 0:
+            line['runtime'] = 0
         res.append(line)
     return res
 
-def generateTaskfile(inData,itr,args):
-    rVals = getRandomValues(len(inData),args.drawFunc,args.expect,args.shape)
-    outData = updateData(copy.deepcopy(inData),rVals,args.treatment,args.expect)
-    fileName = "{0}_{1}_{2}-{3}-{4}_{5}".format(os.path.splitext(os.path.basename(args.inputFile))[0],args.treatment,args.drawFunc,args.expect,args.shape,itr)
-    writeFile(args.path+"/"+fileName+".tasks",outData)
-    print "SIM_ARG 3:{0} {1}/{0}.tasks".format(fileName,args.path)
+def generateTaskfile(inData, itr, args):
+    rVals = getRandomValues(len(inData), args.drawFunc, args.expect, args.shape)
+    outData = updateData(copy.deepcopy(inData), rVals, args.treatment, args.expect)
+    fileName = "{0}_{1}_{2}-{3}-{4}_{5}".format(os.path.splitext(os.path.basename(args.inputFile))[0], args.treatment, args.drawFunc, args.expect, args.shape, itr)
+    writeFile(args.path+"/"+fileName+".tasks", outData)
+    print("SIM_ARG 3:{0} {1}/{0}.tasks".format(fileName, args.path))
 
 
 def main():
     parser = argparse.ArgumentParser(description="Generates Simschlouder job files and the corresponding lab file")
-    parser.add_argument("-f","--file",action='store',required=True,type=str,dest='inputFile',
-            help="The source simschlouder jobfile")
-    parser.add_argument("-g","--genrator",action="store",required=True,type=str,dest="drawFunc",
-            choices=["weibull","normal"],
-            help="The type of random generator to use : weibull,or normal")
-    parser.add_argument("-e","--expectation",action="store",required=True,type=float,dest="expect",
-            help="Expectation or scale of the random process")
-    parser.add_argument("-s","--shape","--sd",action="store",required=True,type=float,dest="shape",
-            help="Shape/Standard Deviation or otherwise second moment of the random process")
-    parser.add_argument("-t","--treatment",action="store",required=True,type=str,dest="treatment",
-            choices=["raw","add","norm"],
-            help="Way the random values are used : RAW, ADDed to the prevision, or \"NORMalized\" (prevision*(expect/rand_val))")
-    parser.add_argument("-n","--number",action="store",required=False,type=int,dest="num",default=1,
-            help="Number of taskfile to generate")
-    parser.add_argument("-p","--path",action="store",required=False,type=str,dest="path",default="./",
-            help="Path to store taskfiles")
+    parser.add_argument("-f", "--file", action='store', required=True, type=str, dest='inputFile',
+                        help="The source simschlouder jobfile")
+    parser.add_argument("-g", "--genrator", action="store", required=True, type=str, dest="drawFunc",
+                        choices=["weibull", "normal"],
+                        help="The type of random generator to use : weibull,or normal")
+    parser.add_argument("-e", "--expectation", action="store", required=True, type=float, dest="expect",
+                        help="Expectation or scale of the random process")
+    parser.add_argument("-s", "--shape", "--sd", action="store", required=True, type=float, dest="shape",
+                        help="Shape/Standard Deviation or otherwise second moment of the random process")
+    parser.add_argument("-t", "--treatment", action="store", required=True, type=str, dest="treatment",
+                        choices=["raw", "add", "norm"],
+                        help="Way the random values are used : RAW, ADDed to the prevision, or \"NORMalized\" (prevision*(expect/rand_val))")
+    parser.add_argument("-n", "--number", action="store", required=False, type=int, dest="num", default=1,
+                        help="Number of taskfile to generate")
+    parser.add_argument("-p", "--path", action="store", required=False, type=str, dest="path", default="./",
+                        help="Path to store taskfiles")
     args = parser.parse_args()
 
     inData = loadFile(args.inputFile)
-    fileList = []
     for i in range(args.num):
-        generateTaskfile(inData,i,args)
+        generateTaskfile(inData, i, args)
 
 
 
 if __name__ == '__main__':
     main()
-
